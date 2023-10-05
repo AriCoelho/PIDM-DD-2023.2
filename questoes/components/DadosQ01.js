4f7e2b14c13c1800a7ec407de95353dff145de53
AriCoelho <ariadinecoelho.f@gmail.com>
2023-10-05 16:18:46 -0300

    prova

--- questoes/components/DadosQ01.js
+++ questoes/components/DadosQ01.js
@@ -1,36 +1,37 @@
 
 const data = [
     {
+      
       title: 'Terça',data: [
         {
-          name: 'Pinheiro',time: '10:00 AM',value: 'R$50.00',
+          name: 'Pinheiro',time: '10:00 AM',value: 'R$50.00', icon:"cart",
         },
         {
-          name: 'Netflix.Com',time: '11:30 AM',value: 'R$35.00',
+          name: 'Netflix.Com',time: '11:30 AM',value: 'R$35.00', icon:"tools"
         },
       ],
     },
     {
       title: '25 de Set',data: [
         {
-          name: 'Drogasil',time: '09:45 AM',value: 'R$20.00',
+          name: 'Drogasil',time: '09:45 AM',value: 'R$20.00', icon:"medical-bag"
         },
       ],
     },
     {
       title: '24 de Set',data: [
         {
-          name: 'Pag*Bodegas',time: '03:30 PM',value: 'R$75.25',
+          name: 'Pag*Bodegas',time: '03:30 PM',value: 'R$75.25', icon:"cart"
         },
         { 
-          name: 'Pag*Bodegas', time: '08:00 AM',value: 'R$15.75',
+          name: 'Pag*Bodegas', time: '08:00 AM',value: 'R$15.75', icon:"cart"
         }
       ],
     },
     {
       title: '21 de Set', data: [
         {
-          name: 'Pinheiro', time: '01:45 PM', value: 'R$60.90',
+          name: 'Pinheiro', time: '01:45 PM', value: 'R$60.90', icon:"cart"
         },
       ],
     },
--- questoes/components/Questao01.jsx
+++ questoes/components/Questao01.jsx
@@ -2,27 +2,37 @@ import React from 'react';
 import { View, Text, SectionList } from 'react-native';
 import data from './DadosQ01';
 import { styles } from './styles';
+import { PaperProvider, IconButton, MD3Colors } from "react-native-paper"
 
 const MySectionList = () => {
   return (
-    <View style={styles.container}>
-      <SectionList
-        sections={data}
-        keyExtractor={(item, index) => item + index}
-        renderItem={({ item }) => (
-          <View style={styles.itemContainer}>
-            <Text style={styles.whiteText}>{item.name}</Text>
-            <Text style={[styles.whiteText]}>{item.value}</Text>
-            <Text style={[styles.whiteText]}>{item.time}</Text>
-          </View>
-        )}
-        renderSectionHeader={({ section: { title } }) => (
-          <View style={styles.sectionHeader}>
-            <Text style={styles.whiteText}>{title}</Text>
-          </View>
-        )}
-      />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <SectionList
+          sections={data}
+          keyExtractor={(item, index) => item + index}
+          renderItem={({ item }) => (
+            <View style={styles.itemContainer}>
+              <IconButton
+                icon={item.icon}
+                iconColor={MD3Colors.tertiary0}
+                size={30}
+                onPress={() => console.log('Pressed')}
+                mode="contained"
+              />
+                <Text style={styles.whiteText}>{item.name}</Text>
+                <Text style={[styles.whiteText]}>{item.time}</Text>
+              <Text style={[styles.whiteText]}>{item.value}</Text>
+            </View>
+          )}
+          renderSectionHeader={({ section: { title } }) => (
+            <View style={styles.sectionHeader}>
+              <Text style={styles.whiteText}>{title}</Text>
+            </View>
+          )}
+        />
+      </View>
+    </PaperProvider>
   );
 };
 
